66f66cf2ddd8a071c39a61bb77ddc60d7dbb5609
retax-callpicker <carlosg@callpicker.com>
2023-02-23 19:02:18 -0600

    Update build output folder

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require("path");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: './',
+  outputDir: path.resolve(__dirname, "distnocake"),
 })
